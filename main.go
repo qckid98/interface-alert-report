b467995b961c24f7e9df35dd7aba5ebaa487572d
Ivan Febriand Muhammad <qckid98@gmail.com>
2023-12-07 11:44:32 +0700

    Add JSON parsing for Splunk webhook data

--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
+
+type splunkWebhook struct {
+	Owner      string `json:"owner"`
+	SearchName string `json:"search_name"`
+	ResultLink string `json:"result_link"`
+	Result     Result `json:"result"`
+}
+
+type Result struct {
+	Message  string `json:"status"`
+	Problem  string `json:"unknown_cmd"`
+	Severity string `json:"aseverity"`
+	User     string `json:"user"`
+	Raw      string `json:"_raw"`
+	HostName string `json:"hostname"`
+	Host     string `json:"host"`
+	UnixTime int64  `json:"_time"`
+
+	Time time.Time
+}
 
 func main() {
 	r := gin.Default()
@@ -25,12 +47,24 @@ func main() {
 			return
 		}
 
-		// Process the incoming JSON payload (you can parse it or do other actions)
-		fmt.Println("Received Splunk webhook data:", string(body))
+		// Parse the JSON payload into a splunkWebhook struct
+		var webhook splunkWebhook
+		err = json.Unmarshal(body, &webhook)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Failed to parse JSON payload"})
+			return
+		}
+
+		location := time.FixedZone("Asia/Bangkok", 7*60*60)
+
+		webhook.Result.Time = time.Unix(webhook.Result.UnixTime, 0).In(location)
+
+		fmt.Println(webhook)
 
 		// Respond to Splunk with a success message
 		c.JSON(http.StatusOK, gin.H{"message": "Webhook data received successfully"})
 	})
 
 	r.Run(":8080")
+
 }
