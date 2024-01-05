a9bc481ff106721544fe5b65ad0962a552e0b140
Ivan Febriand <qckid98@gmail.com>
2024-01-05 16:30:12 +0700

    add acknowledge API and fix time

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
-	"strconv"
 	"time"
 
 	"github.com/gin-contrib/cors"
@@ -29,14 +28,26 @@ type Result struct {
 	DateTime  string `json:"date_time"`
 }
 
+type Acknowledge struct {
+	Id       int32  `json:"id"`
+	Name     string `json:"Name"`
+	Link     string `json:"Link"`
+	Severity string `json:"Severity"`
+	Date     string `json:"Date"`
+	Message  string `json:"Message"`
+	Host     string `json:"Host"`
+	Owner    string `json:"Owner"`
+	AckTime  string `json:"AckTime"`
+}
+
 func main() {
 	// Connect to the database
 
 	r := gin.Default()
 	r.Use(cors.Default())
 
-	// db, err := sql.Open("mysql", "root:root1234@tcp(10.62.170.172:3306)/alert_db")
-	db, err := sql.Open("mysql", "root:babihutan123@tcp(localhost:3306)/alert_db")
+	db, err := sql.Open("mysql", "root:root1234@tcp(10.62.170.172:3306)/alert_db")
+	// db, err := sql.Open("mysql", "root:babihutan123@tcp(localhost:3306)/alert_db")
 	if err != nil {
 		panic(err.Error())
 	}
@@ -47,10 +58,11 @@ func main() {
 		panic(err.Error())
 	}
 
-	r.GET("/", func(c *gin.Context) {
-		readAll, err := db.Query("SELECT * FROM alert_db.report")
+	r.POST("/", func(c *gin.Context) {
+		readAll, err := db.Query("SELECT * FROM alert_db.tester_db")
 		if err != nil {
-			panic(err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
 		defer readAll.Close()
@@ -66,8 +78,9 @@ func main() {
 				Message  string
 				Host     string
 				Owner    string
+				id       string
 			)
-			err := readAll.Scan(&Name, &Link, &Severity, &Date, &Message, &Host, &Owner)
+			err := readAll.Scan(&Name, &Link, &Severity, &Date, &Message, &Host, &Owner, &id)
 			if err != nil {
 				panic(err.Error())
 			}
@@ -80,6 +93,7 @@ func main() {
 				"Message":  Message,
 				"Host":     Host,
 				"Owner":    Owner,
+				"id":       id,
 			})
 		}
 
@@ -110,19 +124,71 @@ func main() {
 			}
 		}
 
-		t := time.Now()
-
-		FullDate := strconv.Itoa(t.Year()) + " " + webhook.Result.DateTime
-
-		insert, err := db.Query("INSERT INTO alert_db.report (Name, Link, Severity, Date, Message, Host, Owner) VALUES (?, ?, ?, ?, ?, ?, ?)", webhook.SearchName, webhook.ResultLink, webhook.Result.Severity, FullDate, webhook.Result.Raw, webhook.Result.Host, webhook.Owner)
+		insert, err := db.Query("INSERT INTO alert_db.tester_db (Name, Link, Severity, Date, Message, Host, Owner) VALUES (?, ?, ?, ?, ?, ?, ?)", webhook.SearchName, webhook.ResultLink, webhook.Result.Severity, webhook.Result.DateTime, webhook.Result.Raw, webhook.Result.Host, webhook.Owner)
 		if err != nil {
-			panic(err.Error())
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
 		}
 
 		defer insert.Close()
 
 		// Respond to Splunk with a success message
 		c.JSON(http.StatusOK, gin.H{"message": "Successfully processed Splunk webhook"})
+	})
+
+	r.POST("/acknowledge/:id", func(c *gin.Context) {
+		id := c.Param("id")
+
+		var (
+			Name     string
+			Link     string
+			Severity string
+			Date     string
+			Message  string
+			Host     string
+			Owner    string
+			Id       string
+		)
+
+		ack := db.QueryRow("SELECT * FROM tester_db WHERE id = ?", id)
+
+		err = ack.Scan(&Name, &Link, &Severity, &Date, &Message, &Host, &Owner, &Id)
+		if err == sql.ErrNoRows {
+			c.JSON(http.StatusNotFound, gin.H{"error": "No data found with id number " + id})
+		}
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		datastring := Date
+
+		layout := "2006-01-02 15:04:05"
+
+		parsedTime, err := time.Parse(layout, datastring)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		t := time.Now().UTC()
+
+		difference := t.Sub(parsedTime)
+		ackTime := difference.Minutes()
+
+		insert, err := db.Query("INSERT INTO alert_db.report (Name, Link, Severity, Date, Message, Host, Owner, AckTime) VALUES (?,?,?,?,?,?,?,?)", Name, Link, Severity, Date, Message, Host, Owner, ackTime)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err})
+		}
+
+		defer insert.Close()
+
+		delete, err := db.Query("DELETE FROM tester_db WHERE id = ?", id)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"message": "Error when deleting"})
+		}
+
+		defer delete.Close()
+		c.JSON(http.StatusOK, gin.H{"message": "Successfully Acknowledge an alert " + t.String() + " " + parsedTime.String()})
 	})
 
 	r.Run(":8080")
